9032129381fbb7b02ff7e5ca9711380697b257bf
Kelvin Badoni <63546827+Kdoni@users.noreply.github.com>
2021-08-03 16:40:49 +0100

    Delete js_assessment.js

--- js_assessment.js
+++
@@ -1,8 +0,0 @@
-function diffArray(array1, array2) {
-    return array1
-        .concat(array2)
-        .filter(item =>
-            !array1.includes(item) ||
-            !array2.includes(item));
-}
-console.log(diffArray(["John", "Paul", "Peter", "John"], ["John", "Paul", "Juda", "Peter", "John"]));
